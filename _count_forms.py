bacc2cc89d0822ff9a6b732e20167c8a526c3701
傳播妹 <obaoba0088@gmail.com>
2026-05-29 12:23:21 +0800

    chore: remove debug scripts

--- _count_forms.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
-c=open(r'C:\Users\FH01\.qclaw\workspace-cwapojim0yfmyvq8\Travel-Lab\kansai-pass.html','r',encoding='utf-8').read()
-forms=[i for i in range(len(c)) if c[i:i+20]=='class="lead-inline"']
-print(f'Found {len(forms)} lead-inline forms: {forms}')
-for i,pos in enumerate(forms):
-    ctx=c[pos-30:pos+50]
-    print(f'Form {i+1} @{pos}: {repr(ctx)}')
